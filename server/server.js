7420573e5542f26555c6b97d4c22b8218d58b791
Al Sabri Bhuiyan <65182602+AlSabriBhuiyan0@users.noreply.github.com>
2024-10-13 03:55:22 +0600

    some changes

--- server/server.js
+++ server/server.js
@@ -1,56 +1,59 @@
-const express = require('express');
-const cors = require('cors');
-const { exec } = require('child_process');
-const fs = require('fs');
-const path = require('path');
-const connectDB = require('./db/index.js');
-const authRouter = require("./routes/auth-routes.js")
+const express = require("express");
+const cors = require("cors");
+const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+const connectDB = require("./db/index.js");
+const authRouter = require("./routes/auth-routes.js");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 const corsOptions = {
-    origin: ['http://localhost:3000', 'https://latex-resume-builder.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'],
-    credentials: true,
+  origin: ["http://localhost:3000", "https://latex-resume-builder.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"],
+  credentials: true,
 };
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.post('/generate-pdf', (req, res) => {
-    const latexCode = req.body.latex;
+app.post("/generate-pdf", (req, res) => {
+  const latexCode = req.body.latex;
 
-    const filePath = path.join(__dirname, 'resume.tex');
-    const pdfPath = path.join(__dirname, 'resume.pdf');
-    fs.writeFileSync(filePath, latexCode);
-    exec(`pdflatex -interaction=nonstopmode ${filePath}`, (err, stdout, stderr) => {
+  const filePath = path.join(__dirname, "resume.tex");
+  const pdfPath = path.join(__dirname, "resume.pdf");
+  fs.writeFileSync(filePath, latexCode);
+  exec(
+    `pdflatex -interaction=nonstopmode ${filePath}`,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error(`Error: ${stderr}`);
+        return res.status(500).send("Error generating PDF");
+      }
+      if (!fs.existsSync(pdfPath)) {
+        return res.status(500).send("PDF not generated");
+      }
+      res.download(pdfPath, "resume.pdf", (err) => {
         if (err) {
-            console.error(`Error: ${stderr}`);
-            return res.status(500).send('Error generating PDF');
+          console.error("Error downloading file:", err);
         }
-        if (!fs.existsSync(pdfPath)) {
-            return res.status(500).send('PDF not generated');
-        }
-        res.download(pdfPath, 'resume.pdf', (err) => {
-            if (err) {
-                console.error('Error downloading file:', err);
-            }
-            fs.unlink(filePath, (err) => {
-                if (err) console.error('Error deleting .tex file:', err);
-            });
-            fs.unlink(pdfPath, (err) => {
-                if (err) console.error('Error deleting .pdf file:', err);
-            });
+        fs.unlink(filePath, (err) => {
+          if (err) console.error("Error deleting .tex file:", err);
         });
-    });
+        fs.unlink(pdfPath, (err) => {
+          if (err) console.error("Error deleting .pdf file:", err);
+        });
+      });
+    }
+  );
 });
 
 // Connection to DB:
 connectDB();
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 // Use the below route to use the auth feature
