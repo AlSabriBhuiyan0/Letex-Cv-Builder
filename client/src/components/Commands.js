019f4f706059d6f0d5517377b26478172e2aceaa
Al Sabri Bhuiyan <65182602+AlSabriBhuiyan0@users.noreply.github.com>
2024-10-14 06:50:17 +0600

    Fix Commands component import issue

---
+++ client/src/components/Commands.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Commands = () => {
+  return (
+    <div>
+      <h1>LaTeX Commands</h1>
+      {/* Add your commands content here */}
+    </div>
+  );
+};
+
+export default Commands;
+
